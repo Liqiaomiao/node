db7fe514908bcc2549fa35bc50000b653a7d04c7
Liqiaomiao <1049259456@qq.com>
2019-12-08 12:46:59 +0800

    create event emitter:pub-sub example - someone leave the chartroom;add when  net close emit leave method

--- node-in-action/eventEmitter/2-11-pub-sub.js
+++ node-in-action/eventEmitter/2-11-pub-sub.js
@@ -12,11 +12,18 @@ channel.on('join', function (id, client) {
     }
     this.on('boardcast', this.subscription[id])
 })
+channel.on('leave', function (id) {
+    channel.removeListener('boardcast', this.subscription[id])
+    channel.emit('boardcast', id, `${ id } has left the chartroom. \n`)
+})
 const server = net.createServer(client => {
     const id = `${ client.remoteAddress }:${ client.remotePort }`
     channel.emit('join', id, client)
     client.on('data', (data) => {
         channel.emit('boardcast', id, data)
+    })
+    client.on('close', () => {
+        channel.emit('leave', id)
     })
 })
 server.listen(8888)
