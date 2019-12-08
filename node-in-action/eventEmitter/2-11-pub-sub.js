13b4be9208275f1d64b9e0aa37d7d29e5ae94f98
Liqiaomiao <1049259456@qq.com>
2019-12-08 13:01:33 +0800

    create event emitter:pub-sub example - remove all listeners

--- node-in-action/eventEmitter/2-11-pub-sub.js
+++ node-in-action/eventEmitter/2-11-pub-sub.js
@@ -16,10 +16,18 @@ channel.on('leave', function (id) {
     channel.removeListener('boardcast', this.subscription[id])
     channel.emit('boardcast', id, `${ id } has left the chartroom. \n`)
 })
+channel.on('shutdown', function () {
+    channel.emit('boardcast', '', 'the server has  shut down.\n')
+    channel.removeAllListeners('boardcast')
+})
 const server = net.createServer(client => {
     const id = `${ client.remoteAddress }:${ client.remotePort }`
     channel.emit('join', id, client)
     client.on('data', (data) => {
+        console.log('data', data.toString());
+        if (data.toString() === 'shut down\r\n') {
+            channel.emit('shutdown')
+        }
         channel.emit('boardcast', id, data)
     })
     client.on('close', () => {
