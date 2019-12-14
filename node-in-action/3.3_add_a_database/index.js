d1e6b03575278668e7cf2715e8758e23c93bf4ce
Liqiaomiao <1049259456@qq.com>
2019-12-14 20:17:52 +0800

    Supporting multiple formats(html,json)

--- node-in-action/3.3_add_a_database/index.js
+++ node-in-action/3.3_add_a_database/index.js
@@ -4,38 +4,46 @@ const bodyParser = require('body-parser')
 const read = require('node-readability')
 const app = express()
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 console.log(Article)
-app.get('/articles',(req,res,next)=>{ // cur http://localhost:3000/articles/
-    Article.all((err,articles)=>{
-        if(err) return next(err)
-        res.send(articles)
+app.get('/articles', (req, res, next) => { // cur http://localhost:3000/articles/
+    Article.all((err, articles) => {
+        if (err) return next(err)
+
+        res.format({
+            html: () => {
+                res.render('articles.ejs', { articles: articles });
+            },
+            json: () => {
+                res.send(articles);
+            }
+        })
     })
 })
-app.post('/articles',(req,res,next)=>{ // curl --data 'url=http://politics.people.com.cn/n1/2019/1214/c1001-31505998.html' http://localhost:3000/articles
+app.post('/articles', (req, res, next) => { // curl --data 'url=http://politics.people.com.cn/n1/2019/1214/c1001-31505998.html' http://localhost:3000/articles
     const url = req.body.url
-    read(url,(err,result)=>{
-        if(err || !result) res.status(500).send('error downloading articles')
+    read(url, (err, result) => {
+        if (err || !result) res.status(500).send('error downloading articles')
         Article.create({
-            title: result.title,content:result.content
-        },(err,article)=>{
-            if(err) return next(err)
+            title: result.title, content: result.content
+        }, (err, article) => {
+            if (err) return next(err)
             res.send('ok')
         })
     })
 })
-app.get('/articles/:id',(req,res,next)=>{ // curl  http://localhost:3000/articles/1
-    Article.find(req.params.id,(err,article)=>{
-        if(err) next(err)
+app.get('/articles/:id', (req, res, next) => { // curl  http://localhost:3000/articles/1
+    Article.find(req.params.id, (err, article) => {
+        if (err) next(err)
         res.send(article)
     })
 })
-app.delete('/articles/:id',(req,res,next)=>{ // curl -X DELETE http://localhost:3000/articles/1
-    Article.delete(req.params.id,(err,article)=>{
+app.delete('/articles/:id', (req, res, next) => { // curl -X DELETE http://localhost:3000/articles/1
+    Article.delete(req.params.id, (err, article) => {
         if (err) return next(err);
         res.send({ message: 'Deleted' });
     })
 })
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log(`App started on port 3000`)
 })
