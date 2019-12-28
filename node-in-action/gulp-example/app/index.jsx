32bca713f8d5ac9710e2e48d60d926b411e0d72a
Liqiaomiao <1049259456@qq.com>
2019-12-28 15:47:01 +0800

    用 Babel 处理 ES2015 和 React 的 gulpfile get:1.gulp 2.gulp-watch

---
+++ node-in-action/gulp-example/app/index.jsx
@@ -0,0 +1,6 @@
+import React from 'reat'
+import ReactDOM from 'react-dom'
+ReactDOM.render(
+    (<div><h1>hello ,world!</h1><span>kxk</span></div>),
+    document.getElementById('example')
+)
---
+++ node-in-action/gulp-example/gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const watch = require('gulp-watch');
+const defaultTask = ()=>{
+    gulp.src('app/*.jsx') // 用gulp自带的文件聚焦工具 gulp.src 查找所有的React jsx文件
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015','react']
+        }))
+        .pipe(concat('all.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+}
+gulp.task('default', () =>
+    defaultTask()
+)
+gulp.task('watch', () => {
+    watch('app/*.jsx', ()=>{
+        console.log('changing')
+        defaultTask()
+    });
+});
+
