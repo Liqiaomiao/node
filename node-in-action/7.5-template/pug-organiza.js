8acdee1cfdd1957719b043643f2cf6c94abd8eee
Liqiaomiao <1049259456@qq.com>
2020-01-08 15:25:46 +0800

    用块追加再加载一个javascript文件

--- node-in-action/7.5-template/pug-organiza.js
+++ node-in-action/7.5-template/pug-organiza.js
@@ -1,5 +1,7 @@
 const pug = require('pug')
 const fs = require('fs')
+const http= require('http')
+const Path = require('path')
 const templagteFile = './templates/page.pug'
 const iterTemplate = fs.readFileSync(templagteFile)
 const context = {
@@ -9,4 +11,10 @@ const context = {
     ]
 }
 const iterFn = pug.compile(iterTemplate,{filename:templagteFile})
-console.log(iterFn(context));
+http.createServer((req, res) => {
+    if (req.url === '/jq.js') {
+        fs.createReadStream(Path.resolve(__dirname, './templates' + req.url)).pipe(res);
+    } else if (req.url === '/') {
+        res.end(iterFn(context))
+    }
+}).listen(3000)
---
+++ node-in-action/7.5-template/templates/jq.js
@@ -0,0 +1,6 @@
+$(function() {
+    $("#message_1").dialog({
+        height: 140,
+        modal: true
+    });
+});
